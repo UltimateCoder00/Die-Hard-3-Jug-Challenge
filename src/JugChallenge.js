21f7f6197ca5be8f2c41a43ed7de99dd178b3770
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-08-11 14:19:36 +0100

    Add weighFiveLitreJug function

--- src/JugChallenge.js
+++ src/JugChallenge.js
@@ -2,8 +2,13 @@
 
 function JugChallenge() {
   this._threeLitreJug = 0
+  this._fiveLitreJug = 0
 }
 
 JugChallenge.prototype.weighThreeLitreJug = function() {
   return this._threeLitreJug;
+};
+
+JugChallenge.prototype.weighFiveLitreJug = function() {
+  return this._fiveLitreJug;
 };
