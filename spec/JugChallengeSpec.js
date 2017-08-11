3ad2d332a390f01b6dfd16ed05cab99152bdfdc0
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-08-11 15:03:03 +0100

    Add Pour from 3 litre jug to 5 litre jug test

--- spec/JugChallengeSpec.js
+++ spec/JugChallengeSpec.js
@@ -7,67 +7,76 @@ describe('JugChallenge', function() {
     jugChallenge = new JugChallenge();
   });
 
-  describe('Weigh Jugs', function() {
-    it('Weigh 3 Litre Jug', function() {
+  describe('Weigh jugs', function() {
+    it('Weigh 3 litre jug', function() {
       expect(jugChallenge.weighThreeLitreJug()).toEqual(0);
     });
 
-    it('Weigh 5 Litre Jug', function() {
+    it('Weigh 5 litre jug', function() {
       expect(jugChallenge.weighFiveLitreJug()).toEqual(0);
     });
   });
 
-  describe('Fill Jugs', function() {
-    it('Fill 3 Litre Jug with three litres', function() {
+  describe('Fill jugs', function() {
+    it('Fill 3 litre jug with three litres', function() {
       jugChallenge.fillThreeLitreJug();
       expect(jugChallenge.weighThreeLitreJug()).toEqual(3);
     });
 
-    it('Fill 5 Litre Jug with five litres', function() {
+    it('Fill 5 litre jug with five litres', function() {
       jugChallenge.fillFiveLitreJug();
       expect(jugChallenge.weighFiveLitreJug()).toEqual(5);
     });
   });
 
-  describe('Empty Jugs', function() {
-    it('Empty 3 Litre Jug', function() {
+  describe('Empty jugs', function() {
+    it('Empty 3 litre jug', function() {
       jugChallenge.fillThreeLitreJug();
       jugChallenge.emptyThreeLitreJug();
       expect(jugChallenge.weighThreeLitreJug()).toEqual(0);
     });
 
-    it('Empty 5 Litre Jug', function() {
+    it('Empty 5 litre jug', function() {
       jugChallenge.fillFiveLitreJug();
       jugChallenge.emptyFiveLitreJug();
       expect(jugChallenge.weighFiveLitreJug()).toEqual(0);
     });
   });
 
-  describe('Are the Jugs Full?', function() {
-    it('Check if 3 Litre Jug is full', function() {
+  describe('Are the jugs full?', function() {
+    it('Check if 3 litre jug is full', function() {
       expect(jugChallenge.isThreeLitreJugFull()).toBeFalsy();
       jugChallenge.fillThreeLitreJug();
       expect(jugChallenge.isThreeLitreJugFull()).toBeTruthy();
     });
 
-    it('Check if 5 Litre Jug is full', function() {
+    it('Check if 5 litre jug is full', function() {
       expect(jugChallenge.isFiveLitreJugFull()).toBeFalsy();
       jugChallenge.fillFiveLitreJug();
       expect(jugChallenge.isFiveLitreJugFull()).toBeTruthy();
     });
   });
 
-  describe('Are the Jugs Empty?', function() {
-    it('Check if 3 Litre Jug is empty', function() {
+  describe('Are the jugs empty?', function() {
+    it('Check if 3 litre jug is empty', function() {
       expect(jugChallenge.isThreeLitreJugEmpty()).toBeTruthy();
       jugChallenge.fillThreeLitreJug();
       expect(jugChallenge.isThreeLitreJugEmpty()).toBeFalsy();
     });
 
-    it('Check if 5 Litre Jug is empty', function() {
+    it('Check if 5 litre jug is empty', function() {
       expect(jugChallenge.isFiveLitreJugEmpty()).toBeTruthy();
       jugChallenge.fillFiveLitreJug();
       expect(jugChallenge.isFiveLitreJugEmpty()).toBeFalsy();
+    });
+  });
+
+  describe('Pour from one jug to the other jug', function() {
+    it('Pour from 3 litre jug to 5 litre jug', function() {
+      jugChallenge.fillThreeLitreJug();
+      jugChallenge.pourIntoFiveLitreJug();
+      expect(jugChallenge.isThreeLitreJugEmpty()).toBeTruthy();
+      expect(jugChallenge.weighFiveLitreJug()).toEqual(3);
     });
   });
 });
