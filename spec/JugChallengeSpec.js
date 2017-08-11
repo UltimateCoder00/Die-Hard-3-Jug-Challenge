7f5095067cfc8c9985ed78ac46a2f4b1498f3f09
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-08-11 14:23:08 +0100

    Add Fill Jugs tests

--- spec/JugChallengeSpec.js
+++ spec/JugChallengeSpec.js
@@ -16,4 +16,16 @@ describe('JugChallenge', function() {
       expect(jugChallenge.weighFiveLitreJug()).toEqual(0);
     });
   });
+
+  describe('Fill Jugs', function() {
+    it('Fill 3 Litre Jug with three litres', function() {
+      jugChallenge.fillThreeLitreJug();
+      expect(jugChallenge.weighThreeLitreJug()).toEqual(3);
+    });
+
+    it('Fill 5 Litre Jug with five litres', function() {
+      jugChallenge.fillFiveLitreJug();
+      expect(jugChallenge.weighFiveLitreJug()).toEqual(5);
+    });
+  });
 });
