c148330624b848a3c2af6d472549ae34baa3a737
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-08-13 22:58:28 +0100

    Update

--- dieHard3JugChallenge.js
+++ dieHard3JugChallenge.js
@@ -1,12 +1,28 @@
-var httpServer = require("http-server");
-var path = require("path");
+// var httpServer = require("http-server");
+// var path = require("path");
+//
+// var pathToHtmlAndJsFiles = path.join(__dirname, "/");
+// var server = httpServer.createServer({root: pathToHtmlAndJsFiles
+// });
+//
+//
+// console.log("Server running at localhost:5000")
+// console.log("'ctrl-c' to end")
+//
+// server.listen(5000);
 
-var pathToHtmlAndJsFiles = path.join(__dirname, "/");
-var server = httpServer.createServer({root: pathToHtmlAndJsFiles
+
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/'));
+
+app.get('/', function(request, response) {
+  response.render('index');
 });
 
-
-console.log("Server running at localhost:5000")
-console.log("'ctrl-c' to end")
-
-server.listen(5000);
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
