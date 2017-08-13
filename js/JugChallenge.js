ebfd99540a56d1c5980ddabd2a1dd049e1d4f513
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-08-13 13:13:11 +0100

    Add functionality to change jug images based on clicks

--- js/JugChallenge.js
+++ js/JugChallenge.js
@@ -43,26 +43,44 @@ $( document ).ready(function() {
   });
 
   $( "#jug1tap-button" ).click(function( event ) {
+    $( "#empty-jug1" ).hide();
+    $( "#half-full-jug1" ).hide();
+    $( "#full-jug1" ).show();
     jugChallenge.fillThreeLitreJug();
   });
 
   $( "#jug1empty-button" ).click(function( event ) {
+    $( "#empty-jug1" ).show();
+    $( "#half-full-jug1" ).hide();
+    $( "#full-jug1" ).hide();
     jugChallenge.emptyThreeLitreJug();
   });
 
   $( "#jug1pouring-button" ).click(function( event ) {
+    $( "#empty-jug1" ).hide();
+    $( "#half-full-jug1" ).show();
+    $( "#full-jug1" ).hide();
     jugChallenge.pourIntoFiveLitreJug();
   });
 
   $( "#jug2tap-button" ).click(function( event ) {
+    $( "#empty-jug2" ).hide();
+    $( "#half-full-jug2" ).hide();
+    $( "#full-jug2" ).show();
     jugChallenge.fillFiveLitreJug();
   });
 
   $( "#jug2empty-button" ).click(function( event ) {
+    $( "#empty-jug2" ).show();
+    $( "#half-full-jug2" ).hide();
+    $( "#full-jug2" ).hide();
     jugChallenge.emptyFiveLitreJug();
   });
 
   $( "#jug2pouring-button" ).click(function( event ) {
+    $( "#empty-jug2" ).hide();
+    $( "#half-full-jug2" ).show();
+    $( "#full-jug2" ).hide();
     jugChallenge.pourIntoThreeLitreJug();
   });
 });
