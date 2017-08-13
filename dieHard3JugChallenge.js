0a147d789c2b112c5f6c8cb4562f9d014a484eb8
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-08-13 23:58:11 +0100

    Update

--- dieHard3JugChallenge.js
+++ dieHard3JugChallenge.js
@@ -1,17 +1,3 @@
-// var httpServer = require("http-server");
-// var path = require("path");
-//
-// var pathToHtmlAndJsFiles = path.join(__dirname, "/");
-// var server = httpServer.createServer({root: pathToHtmlAndJsFiles
-// });
-//
-//
-// console.log("Server running at localhost:5000")
-// console.log("'ctrl-c' to end")
-//
-// server.listen(5000);
-
-
 var express = require('express');
 var app = express();
 
