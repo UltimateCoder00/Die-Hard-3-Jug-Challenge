fe7d8b360f179deb94e2e593aecfe049c281f092
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-08-13 19:28:55 +0100

    Add node files

---
+++ dieHard3JugChallenge.js
@@ -0,0 +1,12 @@
+var httpServer = require("http-server");
+var path = require("path");
+
+var pathToHtmlAndJsFiles = path.join(__dirname, "/");
+var server = httpServer.createServer({root: pathToHtmlAndJsFiles
+});
+
+
+console.log("Server running at localhost:8000")
+console.log("'ctrl-c' to end")
+
+server.listen(8000);
